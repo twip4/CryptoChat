1444ccc025ecdf19f38e07c0d263b6be90eb9324
Twip4 <paulbaudinot@gmail.com>
2024-03-14 16:10:25 +0100

    chat fonctionnel

--- cargo/src/main.rs
+++ cargo/src/main.rs
@@ -1,63 +1,101 @@
-use std::io;
-use std::io::{Read, Write};
-use std::net::{TcpStream};
+use std::process;
+use tokio::io::{self, AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpStream;
+use tokio::select;
 
 struct User {
     login: String,
     password: String,
 }
 
-fn connexion() -> User{
+fn connexion() -> User {
+    let mut tmp_login = String::new();
+    let mut tmp_password = String::new();
 
-    let mut tmp_login= String::new();
-    let mut tmp_pasword = String::new();
-
-    println!("Please enter your login :");
-
-    io::stdin()
+    println!("Please enter your login:");
+    std::io::stdin()
         .read_line(&mut tmp_login)
         .expect("Failed to read line");
+    tmp_login = tmp_login.trim().to_string();
 
-    println!("Please enter your password :");
-
-    io::stdin()
-        .read_line(&mut tmp_pasword)
+    println!("Please enter your password:");
+    std::io::stdin()
+        .read_line(&mut tmp_password)
         .expect("Failed to read line");
+    tmp_password = tmp_password.trim().to_string();
 
-    let user = User {
+    User {
         login: tmp_login,
-        password: tmp_pasword
-    };
-
-    return user
+        password: tmp_password,
+    }
 }
 
-fn main() {
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
     println!("Welcome to Crypto Chat");
 
-    let user = connexion();
+    let mut user: User;
 
-    let mut stream = TcpStream::connect("127.0.0.1:7878").expect("error connextion");
-    println!("Successfully connected to server Crypto Chat");
-    let msg = user.login.as_bytes();
-    stream.write(msg).expect("TODO: panic message");
-    let msg = user.password.as_bytes();
-    stream.write(msg).expect("TODO: panic message");
+    let mut stream = TcpStream::connect("127.0.0.1:7878").await.expect("error connection");
 
     loop {
-        let mut buffer = [0; 1024];
-        match stream.read(&mut buffer) {
-            Ok(bytes_read) => {
-                // Only consider the part of the buffer that contains data
-                let buffer = &buffer[..bytes_read];
-                let message = String::from_utf8_lossy(buffer);
-                if !message.is_empty() {
-                    println!("{}", message);
-                }
-            },
-            Err(e) => {
-                println!("Failed to receive data: {}", e);
-            }
+
+        println!("Successfully connected to server in Crypto Chat");
+
+        user = connexion();
+
+        let credentials = format!("${}:{}$", user.login, user.password);
+        stream.write_all(credentials.as_bytes()).await.expect("Failed to send credentials");
+
+        let mut response = [0; 1024];
+        let bytes_read = stream.read(&mut response).await.expect("Failed to read from stream");
+
+        if bytes_read == 0 {
+            println!("La connexion a été fermée par le serveur.");
+            process::exit(0);
+        }
+
+        let message = String::from_utf8_lossy(&response[..bytes_read]);
+
+        if message.trim() == "OK" {
+            println!("Authentification réussie.");
+            break;
+        } else {
+            println!("Échec de l'authentification. Connexion fermée.");
+            stream.shutdown().await?;
         }
     }
+
+
+
+    let mut stdin = BufReader::new(io::stdin());
+    let mut buffer = vec![0; 1024];
+    let mut stdin_buffer = String::new();
+
+    loop {
+        select! {
+            bytes_read = stream.read(&mut buffer) => {
+                let bytes_read = bytes_read?;
+                if bytes_read == 0 {
+                    println!("Server closed the connection.");
+                    break;
+                }
+                println!("Received: {}", String::from_utf8_lossy(&buffer[..bytes_read]));
+            },
+            read_result = stdin.read_line(&mut stdin_buffer) => {
+                let bytes_read = read_result?;
+                if bytes_read == 0 {
+                    println!("EOF on stdin, stopping.");
+                    break;
+                }
+                if !stdin_buffer.trim().is_empty() {
+                    stream.write_all(stdin_buffer.as_bytes()).await?;
+                    stdin_buffer.clear();
+                }
+            },
+        }
+    }
+
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,31 +1,144 @@
+use std::borrow::Cow;
 use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::net::{Shutdown, SocketAddr, TcpListener, TcpStream};
+use std::sync::Arc;
+use tokio::sync::{Mutex as AsyncMutex, Mutex};
 
-async fn handle_client(mut stream : TcpStream){
-    let mut buffer = [0 ; 1024];
-    stream.read(&mut buffer).expect("buffer error");
-    let message = String::from_utf8_lossy(&buffer);
-    println!("message: {}",message);
-    let reponse = b"Hello you are connected !";
-    stream.write(reponse).expect("TODO: panic message");
+
+#[derive(Debug)]
+struct Client{
+    addr: SocketAddr,
+    stream : TcpStream,
+    connected: bool,
 }
 
-#[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+impl PartialEq for Client {
+    fn eq(&self, other: &Self) -> bool {
+        self.addr == other.addr
+    }
+}
 
-    for stream in listener.incoming(){
-            match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                handle_client(stream).await;
+impl Clone for Client {
+    fn clone(&self) -> Self {
+        Client{
+            addr : self.addr.clone(),
+            stream: self.stream.try_clone().expect("clone failed..."),
+            connected : self.connected
+        }
+    }
+}
+
+fn auth(credentials: Cow<str>) -> bool {
+    let trimmed = credentials.trim_matches('$');
+    let mut parts = trimmed.split(':');
+
+    // Utilisez match pour déballer les valeurs de parts.next() et comparer
+    match (parts.next(), parts.next()) {
+        (Some("polo"), Some("mdp")) => true,
+        _ => false,
+    }
+}
+
+
+async fn handle_client(mut stream: TcpStream, clients: Arc<AsyncMutex<Vec<Client>>>) {
+    let addr = stream.peer_addr().unwrap();
+    let me = Client {
+        addr,
+        stream: stream.try_clone().expect("clone failed..."), // Note: Cloning stream for async TCPStream needs reevaluation
+        connected: true,
+    };
+    {
+        let mut clients = clients.lock().await;
+        clients.push(me.clone());
+    }
+
+    let mut count_trial = 0;
+    while count_trial < 3 {
+        let mut response = vec![0; 1024];
+        match stream.read(&mut response) {
+            Ok(bytes_read) => {
+                if bytes_read == 0 {
+                    println!("Connection closed by client {}", addr);
+                    let mut clients_guard = clients.lock().await;
+                    clients_guard.retain(|client| client.addr != addr);
+                    return;
+                }
+                let credentials = String::from_utf8_lossy(&response[..bytes_read]);
+                if auth(credentials.into()) {
+                    println!("Authentication successful {}", addr);
+                    stream.write_all(b" OK \n").expect("Failed to send success message");
+                    break;
+                } else {
+                    println!("Authentication failed {}", addr);
+                    stream.write_all(b" KO \n").expect("Failed to send success message");
+                }
             }
+            Err(_) => {
+                println!("Failed to read credentials from {}", addr);
+                return;
+            }
+        }
+        count_trial += 1;
+    }
+
+    if count_trial == 3 {
+        println!("Connection closed due to failed authentication {}", addr);
+        let _ = stream.shutdown(Shutdown::Both);
+        return;
+    }
+
+    // Main message loop
+    loop {
+        let mut buffer = vec![0; 1024];
+        match stream.read(&mut buffer) {
+            Ok(bytes_read) => {
+                if bytes_read == 0 {
+                    println!("Connection closed by client {}", addr);
+                    break;
+                }
+                let message = String::from_utf8_lossy(&buffer[..bytes_read]);
+                if !message.is_empty() {
+                    broadcast_write(me.clone(), clients.clone(), message.to_string()).await;
+                    println!("Message from {}: {}", addr, message);
+                }
+            },
             Err(e) => {
-                eprintln!("Error: {}", e);
+                println!("Failed to receive data from {}: {}", addr, e);
+                break;
             }
         }
     }
+    let mut clients_guard = clients.lock().await;
+    clients_guard.retain(|client| client.addr != addr);
+}
 
+async fn broadcast_write(emeteur: Client, clients: Arc<Mutex<Vec<Client>>>, msg: String) {
+    let mut clients = clients.lock().await;
+    for client in &mut *clients {
+        if client.addr != emeteur.addr {
+            client.stream.write(msg.as_bytes()).expect("TODO: panic message");
+        }
+    }
+}
+#[tokio::main]
+async fn main() {
+    let clients = Arc::new(AsyncMutex::new(Vec::new()));
 
-drop(listener);
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    loop {
+        let (stream, _addr) = match listener.accept() {
+            Ok((stream, addr)) => (stream, addr),
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                continue;
+            }
+        };
+
+        let clients = Arc::clone(&clients);
+        println!("New connection: {:?}", clients);
+        tokio::task::spawn(async move {
+            handle_client(stream, clients).await;
+        });
+    }
 }
